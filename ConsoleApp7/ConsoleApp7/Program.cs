c2f7f1d11df19c95c3eb65c3bcb80da6664bcc12
Evgeny Kozlov <kozloveg1996@gmail.com>
2019-04-19 13:13:11 +0300

    Improved tool

--- ConsoleApp7/ConsoleApp7/Program.cs
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -2,24 +2,24 @@
 
 namespace ConsoleApp7
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Enter your int value!");
-            int value;
-            int.TryParse(Console.ReadLine(), out value);
+            long value;
+            long.TryParse(Console.ReadLine(), out value);
 
-            if(value != 0)
+            if (value != 0)
             {
                 Console.WriteLine("Your GUID is:");
-                Console.WriteLine(ToGuid(value));
+                Console.WriteLine(value.ToGuid());
             }
 
             Console.ReadLine();
         }
 
-        public static Guid ToGuid(int value)
+        public static Guid ToGuid(this long value)
         {
             byte[] bytes = new byte[16];
             BitConverter.GetBytes(value).CopyTo(bytes, 0);
