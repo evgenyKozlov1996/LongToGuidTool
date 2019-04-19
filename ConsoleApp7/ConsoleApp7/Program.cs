19163819f764b63fe42c34b061eb5154fed7d044
Evgeny Kozlov <kozloveg1996@gmail.com>
2019-04-19 12:58:20 +0300

    Implemented tool

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your int value!");
+            int value;
+            int.TryParse(Console.ReadLine(), out value);
+
+            if(value != 0)
+            {
+                Console.WriteLine("Your GUID is:");
+                Console.WriteLine(ToGuid(value));
+            }
+
+            Console.ReadLine();
+        }
+
+        public static Guid ToGuid(int value)
+        {
+            byte[] bytes = new byte[16];
+            BitConverter.GetBytes(value).CopyTo(bytes, 0);
+            return new Guid(bytes);
+        }
+    }
+}
